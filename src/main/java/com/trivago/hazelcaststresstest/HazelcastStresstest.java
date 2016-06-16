9a858dada99d1deba44f5c9316f3f500c5553367
Christian Esken <Christian.Esken@trivago.com>
2016-06-16 13:26:48 +0200

    Adjust documentation

--- src/main/java/com/trivago/hazelcaststresstest/HazelcastStresstest.java
+++ src/main/java/com/trivago/hazelcaststresstest/HazelcastStresstest.java
@@ -29,7 +29,7 @@ import com.hazelcast.core.IMap;
  * 
  * Making the client die slower:
  * - entryCount = 10_000;
- * - entryCount = 100_000;
+ * - entryCount = 1_000;
  * 
  * Different possibility for OOM:
  * - Remove "implements Serializable" from CacheKeyType. OOM due to allocating many HazelcastSerializationException's
